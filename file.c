02d75ce02f2adde5bec9d8805b2a3d8c930afdbe
Viraj001 <58590684+Viraj001@users.noreply.github.com>
2019-12-06 16:26:48 +0530

    Update file.c

--- file.c
+++ file.c
@@ -11,14 +11,14 @@ int main()
 }
 int convertBinaryToDecimal(long long n)
 {
-    int decimalNumber = 0, i = 0;
-    int remainder;
+    int dNum= 0, i = 0;
+    int r;
     while (n!=0)
     {
-        remainder = n%10;
+        r = n%10;
         n /= 10;
-        decimalNumber += remainder*pow(2,i);
+        dNum += r*pow(2,i);
         ++i;
     }
-    return decimalNumber;
+    return dNum;
 }
